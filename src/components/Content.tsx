a8bc67f7ee220890c96f6b505f357a13c0234f90
GabrielMeloPV <gabriel.melo@pharmaviews.com.br>
2022-05-25 09:55:18 -0300

    componentization Content,Sidebar and Header

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,27 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+import '../styles/content.scss';
+
+import {GenreResponseProps, MovieProps} from '../App'
+import { Header } from "./Header";
+
+interface ContentProps{
+  selectedGenre: GenreResponseProps;
+  movies: MovieProps[];
+}
+
+export function Content({selectedGenre, movies} : ContentProps) {
+  return(
+    <div className="container">
+
+      <Header selectedGenre={selectedGenre} />
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
